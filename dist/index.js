f0e9b0a7fe70365b6915ffc8d09bc4e533c93e40
ryan <ryandsferrer@gmail.com>
2025-05-20 09:39:36 -0300

    rodando build do codigo

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-import itens from './itens.js';
-const app = express();
+import itens from './itens.js'; //importando array de objetos
+const Serverapp = express();
 //ativando cors
-app.use(cors());
-app.get('/products', (req, res) => {
-    let id = req.query.id || undefined;
-    const page = parseInt(req.query._page) || 1; //pegue o numero na variavel na url ou comece com 1
-    const countItens = parseInt(req.query._limit) || itens.products.length; //pegue o limite que a requisicao esta pedindo ou
+Serverapp.use(cors());
+Serverapp.get('/products', (req, res) => {
+    let id = req.query.id || undefined; //identifique a query da url
+    const page = parseInt(req.query._page) || 1; //pegando o numero na variavel da url que representa a pagina atual ou comece com 1
+    const countItens = parseInt(req.query._limit) || itens.products.length; //pegando o limite que a requisicao esta pedindo ou
     //retorne a quantidade do json
-    //verificando se id existe e retornando iten
-    if (id) {
+    if (id) { //verificando se id existe e retornando item especifico
         //encontrar o item
         const arrItem = itens.products.find(prod => prod.id === Number(id));
         res.json(arrItem);
     }
-    const startIndex = (page - 1) * countItens;
-    const endIndex = page * countItens;
+    const Indexstart = (page - 1) * countItens; //definir o ponto de partida do indice
+    const Indexfind = page * countItens; //definir o ponto final do iten
     //dividindo json
-    const paginatedProducts = itens.products.slice(startIndex, endIndex);
+    const paginatedProducts = itens.products.slice(Indexstart, Indexfind);
     //configuracao do header
     res.setHeader("x-total-count", itens.products.length);
     res.json(paginatedProducts);
 });
 //escolha a porta ou use a 3000
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+Serverapp.listen(port, function () {
     console.warn(`Servidor rodando na porta ${port}`);
 });
