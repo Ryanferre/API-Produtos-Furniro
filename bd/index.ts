b76bbb47733c89caa8a49be57717d1bbd6a767e6
ryan <ryandsferrer@gmail.com>
2025-05-20 09:37:44 -0300

    adicionando clareza  aos comentarios

--- bd/index.ts
+++ bd/index.ts
@@ -1,37 +1,37 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors'
-import itens from './itens.js';
+import itens from './itens.js';//importando array de objetos
 
 
-   const app: Express = express();
+   const Serverapp: Express = express();
    //ativando cors
-   app.use(cors())
+   Serverapp.use(cors())
 
-app.get('/products', (req: Request, res: Response): void=>{
-    let id= req.query.id as string || undefined//identifique a query da url
-    const page: number= parseInt(req.query._page as string) || 1 //pegando o numero na variavel da url ou comece com 1
-    const countItens: number= parseInt(req.query._limit as string) || itens.products.length //pegando o limite que a requisicao esta pedindo ou
-                                                                                            //retorne a quantidade do json
+    Serverapp.get('/products', (req: Request, res: Response): void=>{
+        let id= req.query.id as string || undefined //identifique a query da url
+        const page: number= parseInt(req.query._page as string) || 1 //pegando o numero na variavel da url que representa a pagina atual ou comece com 1
+        const countItens: number= parseInt(req.query._limit as string) || itens.products.length //pegando o limite que a requisicao esta pedindo ou
+                                                                                                //retorne a quantidade do json
 
-    if(id){//verificando se id existe e retornando iten
-        //encontrar o item
-        const arrItem= itens.products.find(prod => prod.id === Number(id))
+        if(id){//verificando se id existe e retornando item especifico
+            //encontrar o item
+            const arrItem= itens.products.find(prod => prod.id === Number(id))
 
-       res.json(arrItem)
-    }
-    const Indexstart = (page - 1) * countItens;
-    const Indexfind = page * countItens;
+        res.json(arrItem)
+        }
+        const Indexstart = (page - 1) * countItens;//definir o ponto de partida do indice
+        const Indexfind = page * countItens;//definir o ponto final do iten
 
-    //dividindo json
-    const paginatedProducts = itens.products.slice(Indexstart, Indexfind);
+        //dividindo json
+        const paginatedProducts = itens.products.slice(Indexstart, Indexfind);
 
-    //configuracao do header
-    res.setHeader("x-total-count", itens.products.length);
-    res.json(paginatedProducts)
-})
+        //configuracao do header
+        res.setHeader("x-total-count", itens.products.length);
+        res.json(paginatedProducts)
+    })
 
-//escolha a porta ou use a 3000
-const port = process.env.PORT || 3000;
-app.listen(port, function(){
-    console.warn(`Servidor rodando na porta ${port}`)
-})
+    //escolha a porta ou use a 3000
+    const port = process.env.PORT || 3000;
+    Serverapp.listen(port, function(){
+        console.warn(`Servidor rodando na porta ${port}`)
+    })
