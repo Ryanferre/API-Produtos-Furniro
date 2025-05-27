6d78d26107f90c069a2aced9b602ad2b8348690a
Ryan <ryandsferrer@gmail.com>
2025-05-27 10:13:45 -0300

    reorganization itens in file itens.tsx

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,13 @@ import cors from 'cors';
 import itens from './itens.js'; //importando array de objetos
 const Serverapp = express();
 //ativando cors
-app.use(cors());
-app.get('/products', (req, res) => {
+Serverapp.use(cors());
+Serverapp.get('/products', (req, res) => {
     let id = req.query.id || undefined; //identifique a query da url
-    const page = parseInt(req.query._page) || 1; //pegando o numero na variavel da url ou comece com 1
+    const page = parseInt(req.query._page) || 1; //pegando o numero na variavel da url que representa a pagina atual ou comece com 1
     const countItens = parseInt(req.query._limit) || itens.products.length; //pegando o limite que a requisicao esta pedindo ou
     //retorne a quantidade do json
-    if (id) { //verificando se id existe e retornando iten
+    if (id) { //verificando se id existe e retornando item especifico
         //encontrar o item
         const arrItem = itens.products.find(prod => prod.id === Number(id));
         res.json(arrItem);
